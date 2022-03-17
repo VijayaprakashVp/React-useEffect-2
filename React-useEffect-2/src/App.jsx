02a439527fa034545728dd18de7651c70a48c2ee
VijayaprakashVp <prakashvijay905@gmail.com>
2022-03-17 12:21:06 +0530

    Finished

--- React-useEffect-2/src/App.jsx
+++ React-useEffect-2/src/App.jsx
@@ -4,10 +4,45 @@ import { Timer } from "./components/Timer";
 
 function App() {
   const [show, setShow] = useState(true);
+  const [flag, setFlag] = useState(false);
+  const [start, setStart] = useState(0);
+  const [end, setEnd] = useState(0);
+  // console.log("end:", typeof end);
+  // console.log("start:", typeof start);
 
   return (
     <div className="App">
-      {show ? <Timer /> : ""}
+      <input
+        type="number"
+        // value={start}
+        placeholder="Start Time"
+        onChange={(e) => {
+          // console.log(typeof e.target.value);
+          setStart(+e.target.value);
+        }}
+      />
+      <br />
+      <br />
+      <input
+        type="number"
+        // value={end}
+        placeholder="End Time"
+        onChange={(e) => {
+          setEnd(+e.target.value);
+        }}
+      />
+      <br />
+      <br />
+      <button
+        onClick={() => {
+          setFlag(!flag);
+        }}
+      >
+        Start
+      </button>
+      <br />
+      {show && flag ? <Timer start={start} end={end} flag={flag} /> : ""}
+      <br />
       <button
         onClick={() => {
           setShow(!show);
--- React-useEffect-2/src/components/Timer.jsx
+++ React-useEffect-2/src/components/Timer.jsx
@@ -1,27 +1,27 @@
 import { useEffect, useState } from "react";
 
-export function Timer() {
-  const [timer, setTimer] = useState(10);
+export function Timer({ start, end, flag }) {
+  const [timer, setTimer] = useState(start);
 
   useEffect(() => {
     const id = setInterval(() => {
       setTimer((prev) => {
-        if (prev <= 1) {
+        if (prev >= end) {
           clearInterval(id);
-          return 0;
+          return "Time Up";
         }
-        return prev - 1;
+        return prev + 1;
       });
     }, 1000);
-    return () => {
-      clearInterval(id);
-      console.log("unmounted");
-    };
+    // return () => {
+    //   clearInterval(id);
+    //   console.log("unmounted");
+    // };
   }, []);
   return (
     <div>
-      <h3>Start Timer : 0</h3>
-      <h3>End Timer : 0 </h3>
+      <h3>Start Timer : {start}</h3>
+      <h3>End Timer : {end} </h3>
       <h1>Timer : {timer}</h1>
     </div>
   );
